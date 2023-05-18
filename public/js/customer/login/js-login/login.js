c86c0e5c0d401d5e6ab74b5ac82ee107251a8d1f
Ming <s3938024@rmit.edu.vn>
2023-05-18 16:56:58 +0700

    added validation for signup

--- public/js/customer/login/js-login/login.js
+++ public/js/customer/login/js-login/login.js
@@ -32,70 +32,55 @@ var sUsernameError = document.getElementById('s-username-error');
 var lPasswordError = document.getElementById('l-password-error');
 //signup password
 var sPasswordError = document.getElementById('s-password-error');
-//phone number
-/*var phoneError = document.getElementById('phone-error');*/
 //signup address
-var sAddressError = document.getElementById('s-address-error');
+var AddressError = document.getElementById('s-address-error');
 
 /*validator*/
 
 //name
 function validateName(){
-    //take value from user input to full name
+    //take value from user input
     var name = document.getElementById('contact-name').value;
     //condition 
     //if the slot is empty will display error message
-    if(name.length == 0){
-        nameError.innerHTML = 'full name is required';
+    if(name.length < 8 || name.length < 15){
+        nameError.innerHTML = 'must be between 8 - 15 characters';
         return false;
     }
-    //first character must be uppercase letter
-    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
-        nameError.innerHTML = 'first letter must be uppercase';
-        return false;
-    }
-
     nameError.innerHTML = '<i class="fa-solid fa-check"></i>';
     return true;
 }
 
-/*//phone number
-function validatePhone(){
-    //take value from user input to phone number
-    var phone = document.getElementById('contact-phone').value;
+//address
+function validateAddress(){
+    //take value from user input
+    var address = document.getElementById('contact-s-address').value;
     //condition 
     //if the slot is empty will display error message
-    if(phone.length == 0){
-        phoneError.innerHTML = 'phone number is required';
+    if(AddressError.length < 8 || address.length < 15){
+        AddressError.innerHTML = 'must be between 8 - 15 characters';
         return false;
     }
-    //phone number must be 10 digits
-    if(phone.length !== 10){
-        phoneError.innerHTML = 'phone number should be 10 digits';
-        return false;
-    }
-    //check for number from 0-9 for each digits and total must be 10 digits
-    if(!phone.match(/^[0-9]{10}$/)){
-        phoneError.innerHTML = 'only number';
-        return false;
-    }
-
-    phoneError.innerHTML = '<i class="fa-solid fa-check"></i>';
+    AddressError.innerHTML = '<i class="fa-solid fa-check"></i>';
     return true;
-}*/
+}
 
 //signup username
 function validateSUsername(){
     //take value from user input to email
-    var sEmail = document.getElementById('contact-s-username').value;
+    var sUsername = document.getElementById('contact-s-username').value;
     //condition 
     //if the slot is empty will display error message
-    if(sEmail.length == 0){
+    if(sUsername.length == 0){
         sUsernameError.innerHTML = 'username is required';
         return false;
     }
+    if(sUsername.length < 8 || sUsername.length >15){
+        sUsernameError.innerHTML = 'must be between 8 - 15 characters';
+        return false;
+    }
     //rules
-    if(sEmail.match(/^[A-Za-z]\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+    if(sUsername.match(/^[A-Za-z]\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
         sUsernameError.innerHTML = 'Email Invalid';
         return false;
     }
@@ -104,101 +89,30 @@ function validateSUsername(){
     return true;
 }
 
-//login username
-function validateLUsername(){
-    //take value from user input to email
-    var lEmail = document.getElementById('contact-l-username').value;
-    //condition 
-    //if the slot is empty will display error message
-    if(lEmail.length == 0){
-        lUsernameError.innerHTML = 'Username is required';
-        return false;
-    }
-    //rules
-    if(lEmail.match(/^[A-Za-z]\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
-        lUsernameError.innerHTML = 'Username Invalid';
-        return false;
-    }
-
-    lUsernameError.innerHTML = '<i class="fa-solid fa-check"></i>';
-    return true;
-}
-
 //signup password
 function validateSPassword(){
     //take value from user input to password
-    // var sPassword = document.getElementById('contact-s-password').value;
+    var sPassword = document.getElementById('contact-s-password').value;
     //condition 
     //if the slot is empty will display error message
-    var p = document.getElementById('contact-s-password').value,
-        errors = [];
-    if (p.length < 8) {
-        errors.push("Your password must be at least 8 characters"); 
-    }
-    else if (p.search(/[a-z]/i) < 0) {
-        errors.push("Your password must contain at least one letter.");
-    }
-    else if (p.search(/[0-9]/) < 0) {
-        errors.push("Your password must contain at least one digit."); 
-    }
-    else {
-        alert(errors.join("\n"));
+    if(sPassword.length == 0){
+        sPasswordError.innerHTML = 'Password is required';
         return false;
     }
-    return true;
-    // if(sPassword.length == 0){
-    //     sPasswordError.innerHTML = 'Password is required';
-    //     return false;
-    // }
-    // //if the slot is bellow 8 character
-    // // if(sPassword.length < 8){
-    // //     sPasswordError.innerHTML = 'Password length must be at least 8 characters';
-    // //     return false;
-    // // }
+    
+    //if the slot is bellow 8 character
+    if(sPassword.length < 8){
+        sPasswordError.innerHTML = 'Password length must be at least 8 characters';
+        return false;
+    }
 
-    // var pattern = new RegExp(
-    //     "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"        
-    // );
-
-    // if (pattern.test(sPassword)) {
-    //     return true;
-    //   } else {
-    //     sPasswordError.innerHTML = 'Required 1 number, 1 uppercse, 1 lowercase, 1 special symbol';
-    //     return false;
-    //   }
-
-    // //rules
-    // if(!sPassword.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/)){
-    //     sPasswordError.innerHTML = 'Required 1 number, 1 uppercse, 1 lowercase, 1 special symbol';
-    //     return false;
-    // }
+    //rules
+    if(!sPassword.match(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,20}$/)){
+        sPasswordError.innerHTML = 'invalid format';
+        return false;
+    }
 
     sPasswordError.innerHTML = '<i class="fa-solid fa-check"></i>';
-    return true;
-}
-
-//login password
-function validateLPassword(){
-    //take value from user input to password
-    var lPassword = document.getElementById('contact-l-password').value;
-    //condition 
-    //if the slot is empty will display error message
-    if(lPassword.length == 0){
-        lPasswordError.innerHTML = 'Password is required';
-        return false;
-    }
-    //if the slot is bellow 8 character
-    if(lPassword.length < 8){
-        lPasswordError.innerHTML = 'Password length must be atleast 8 characters';
-        return false;
-    }
-    //rules
-    if(lPassword.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/)){
-        lPasswordError.innerHTML = 'Password must contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character';
-        return false;
-    }
-
-    lPasswordError.innerHTML = '<i class="fa-solid fa-check"></i>';
     return true;
 }
 
